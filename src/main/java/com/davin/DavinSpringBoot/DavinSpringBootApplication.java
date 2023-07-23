2c29a39c3bdb66cd1d4b176c6a970d0c85534788
davinreyhans <[daavinreyhans@gmail.com]>
2023-07-24 00:49:25 +0700

    init

---
+++ src/main/java/com/davin/DavinSpringBoot/DavinSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.davin.DavinSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavinSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavinSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davin/DavinSpringBoot/DavinSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davin.DavinSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavinSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
